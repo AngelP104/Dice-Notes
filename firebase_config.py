734352d29e5a8d3fd95e846e3254f859237b5692
AngelP104 <angel.pardo104@gmail.com>
2025-06-08 11:30:32 +0200

    Cambios uso de credenciales Firebase

--- firebase_config.py
+++ firebase_config.py
@@ -1,3 +1,5 @@
+"""
+# ANTES de despliegue
 import firebase_admin
 from firebase_admin import credentials
 import os
@@ -9,3 +11,18 @@ cred = credentials.Certificate("firebase_config.json")
 
 #firebase_admin.initialize_app(cred)
 default_app = firebase_admin.initialize_app(cred)
+"""
+
+import os
+import json
+from firebase_admin import credentials, initialize_app
+
+firebase_creds = os.environ.get('FIREBASE_CONFIG')
+
+if os.environ.get("FIREBASE_CONFIG"):
+    firebase_dict = json.loads(os.environ["FIREBASE_CONFIG"])
+    cred = credentials.Certificate(firebase_dict)
+else:
+    cred = credentials.Certificate("firebase_config.json")
+
+firebase_app = initialize_app(cred)
